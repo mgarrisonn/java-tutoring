a20b9738f1c318cfa459a97a7c9c4b5dd5f90d44
BranceA <brancealexander@gmail.com>
2020-04-21 14:01:25 -0500

    second part of arraylist explanation

--- src/ArrayListsAndLoops/ArrayListPractice.java
+++ src/ArrayListsAndLoops/ArrayListPractice.java
@@ -8,10 +8,12 @@ public class ArrayListPractice {
 
     public static void main(String[] args) {
         // .add is a method to put new elements into the ArrayList
+
         colors.add("green");
         // ["green"]
 
         // Anything added will be put at the end of the ArrayList
+
         colors.add("blue");
         //["green", "blue"
 
@@ -22,7 +24,30 @@ public class ArrayListPractice {
         // You pass what you want to add to the ArrayList as the second argument
         // We put 1 as the index point. Blue is currently at index 1.
         // Since we said that red goes at index 1 then red goes to index 1 and blue is moved to the next index
+
         colors.add(1, "red");
         // ["green", "red", "blue"]
+
+
+        // Normally we use [index] to access specific elements
+        // ArrayLists are an object that has methods that manipulate an array behind the scenes
+        // Instead of colors[1] to access the String "red" we use colors.get(1)
+
+        String oneColor = colors.get(1);
+        // oneColor = "red"
+        // colors = ["green", "red", "blue"]
+
+        // In Javascript we used pop and shift to remove elements from an array
+        // ArrayLists use the .remove()
+
+        // We pass .remove an index number and it will remove the element at that index
+        colors.remove(2);
+        // colors = ["green", "red"]
+
+
+        // .remove is overloaded so you can remove the an element the first time the element is found
+        colors.remove("red");
+        // colors = ["green"]
+
     }
 }

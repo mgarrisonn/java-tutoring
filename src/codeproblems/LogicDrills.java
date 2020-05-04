19bc709794d7721f856a560c3a28cb1aad853796
BranceA <brancealexander@gmail.com>
2020-05-04 15:38:45 -0500

    added coding problems

---
+++ src/codeproblems/LogicDrills.java
@@ -0,0 +1,50 @@
+package codeproblems;
+
+public class LogicDrills {
+    public static void main(String[] args) {
+
+    }
+
+//    https://medium.com/@codingfreak/top-25-programming-puzzles-and-brain-teasers-dac17b41e94a
+
+//    Add two numbers without using addition operator
+
+//    Find maximum, minimum of three numbers without using conditional statement and/or ternary operator
+
+//    Determine if two integers are equal without using comparison and arithmetic operators
+
+//    Print a colon without using a colon anywhere in the program
+
+//    Find if a number is even or odd without using any conditional statement
+
+
+//    https://www.java67.com/2018/05/top-75-programming-interview-questions-answers.html
+
+//    find the duplicate number on a given integer array
+
+    // in an array of integers from 1 to 10 find the missing number
+
+//    Write a method which will remove any given character from a String
+
+    //https://codingpuzzles.com/floyd-triangle-5231615d5c4d
+
+    //Write a method that accepts an integer and prints that many rows of a Floyd Triangle
+
+// This is what a Floyd Triangle looks like
+//            1
+//            2 3
+//            4 5 6
+//            7 8 9 10
+
+
+    // from Brance
+
+    // Write a function that takes stupid MySQL dates 1234-56-78 and turns them into normal dates 78-56-1234
+
+    // Write a function that accepts an array of strings and a string. if the array doesn't contain the given string add the string to the array. Return the array either way.
+
+    // write a function that accepts a matrix of integers and returns the array from the matrix with the highest sum of all elements
+
+
+
+}
